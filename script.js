86f69a72bd2047cf862a989b47f36421d34de091
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-19 10:10:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function nextPalindrome(num) {
   //your JS code here.
+	num++; 
+    while (!isPalindrome(num)) {
+        num++;
+    }
+    return num;
+}
+function isPalindrome(num) {
+    const str = num.toString();
+    return str === str.split('').reverse().join('');
 }
 
 const input = prompt("Enter a palindrome number");
-alert(nextPalindrome(input));
+alert(nextPalindrome(input))
